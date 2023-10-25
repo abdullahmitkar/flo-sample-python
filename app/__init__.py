9f4d57a4848f832c2565c8d75ceb5af61ea0608f
Abdullah Mitkar <45327508+abdullahmitkar@users.noreply.github.com>
2023-10-24 20:52:58 -0700

    Update __init__.py
    Signed-off-by: Abdullah Mitkar <45327508+abdullahmitkar@users.noreply.github.com>

--- app/__init__.py
+++ app/__init__.py
@@ -85,7 +85,7 @@ def create_app():
     # --------------------------------------------------
     @app.route('/')
     def default_route():
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('Auth.login'))
     @app.route('/googled75d43e26e15ddd9.html')
     @app.route('/sitemap.xml')
     @app.route('/robots.txt')
